e13c12b15b16ad30661bce687c3a81c3a22dfa9c
Jules Amelin <Jules Amelin@momentum-tech.ca>
2025-04-29 10:23:34 -0400

    - refactoring (pagination) - mise en cache avec ocelot - Sécurisation (appli) =Appli : Config OAuth dans Azure (Page de login) - journalisation

--- GateWay.API/Program.cs
+++ GateWay.API/Program.cs
@@ -1,72 +1,81 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Microsoft.OpenApi.Models;
-using MMLib.SwaggerForOcelot.DependencyInjection;
-using MMLib.SwaggerForOcelot.Middleware;
 using CacheManager.Core;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration
+try
+{
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+    builder.Host.UseSerilog();
+
+    builder.Configuration
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Configuration explicite de CacheManager avec un type générique `object`
-builder.Services.AddSingleton<ICacheManager<object>>(CacheFactory.Build(settings =>
-{
-    settings.WithDictionaryHandle(); // Cache en mémoire basé sur un dictionnaire
-}));
-
-
-builder.Services.AddOcelot();
-
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy",
-        builder => builder
-            .AllowAnyOrigin()  
-            .AllowAnyMethod()  
-            .AllowAnyHeader());
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
+    // Configuration explicite de CacheManager avec un type générique `object`
+    builder.Services.AddSingleton<ICacheManager<object>>(CacheFactory.Build(settings =>
     {
-        Title = "Bibliothèque API Gateway",
-        Version = "v1",
-        Description = "API Gateway pour le système de gestion de bibliothèque",
-        Contact = new OpenApiContact
+        settings.WithDictionaryHandle(); 
+    }));
+
+    builder.Services.AddOcelot();
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy",
+            builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+    });
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
         {
-            Name = "Administration Bibliothèque"
-        }
+            Title = "Bibliothèque API Gateway",
+            Version = "v1",
+            Description = "API Gateway pour le système de gestion de bibliothèque",
+            Contact = new OpenApiContact
+            {
+                Name = "Administration Bibliothèque"
+            }
+        });
     });
-});
 
-builder.Services.AddSwaggerForOcelot(builder.Configuration, options =>
-{
-    options.GenerateDocsForGatewayItSelf = true; 
-});
-
-var app = builder.Build();
-
-// Environnement de développement : Activer Swagger et SwaggerForOcelot
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerForOcelotUI(opt =>
+    builder.Services.AddSwaggerForOcelot(builder.Configuration, options =>
     {
-        opt.PathToSwaggerGenerator = "/swagger/docs";
+        options.GenerateDocsForGatewayItSelf = true;
     });
+
+    var app = builder.Build();
+
+    // Environnement de développement : Activer Swagger et SwaggerForOcelot
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerForOcelotUI(opt =>
+        {
+            opt.PathToSwaggerGenerator = "/swagger/docs";
+        });
+    }
+
+    app.UseCors("CorsPolicy");
+    await app.UseOcelot();
+
+    app.Run();
 }
-
-app.UseCors("CorsPolicy"); 
-await app.UseOcelot();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "L'API Gateway n'a pas pu démarrer correctement");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
